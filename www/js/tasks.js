b881f3191b463d0ab5e11f35199354bedd56e2c7
camilogsaenz <117381403+camilogsaenz@users.noreply.github.com>
2024-05-11 15:39:08 -0500

    delete and update

--- www/js/tasks.js
+++ www/js/tasks.js
@@ -8,7 +8,7 @@ function onDeviceReady() {
 
 async function showTasks(tasks) {
     try {
-        const url= 'http://localhost:3050/api/v1/tasks'
+        const url = 'http://localhost:3050/api/v1/tasks';
         const response = await fetch(url, {
             method: 'GET',
             headers: {
@@ -24,17 +24,63 @@ async function showTasks(tasks) {
         if (tasks != undefined && tasks != null && tasks.length > 0) {
             data.data.forEach(function(task) {
                 var newElement = document.createElement("ons-list-item");
+
                 console.log(task);
-                newElement.textContent = task.title;
+
+                var newDiv = document.createElement('div');
+                newDiv.setAttribute('class', 'left');
+                newDiv.textContent = task.title + ' - ' + task.description;
+                newElement.appendChild(newDiv);
+
+                var newDiv2 = document.createElement('div');
+                newDiv2.setAttribute('class', 'right');
+
+                // Crear un contenedor para los iconos
+                var iconContainer = document.createElement('div');
+                iconContainer.style.display = 'flex'; // Para que los iconos estén en línea
+                iconContainer.style.width= "25%"; // Para que los iconos estén en línea
+                iconContainer.style.justifyContent = 'space-between'; // Para alinear los iconos a la derecha
+                iconContainer.style.alignItems = 'center'; // Para centrar verticalmente los iconos
+ 
                 // Crear el botón de modificar
-                var editButton = document.createElement('ons-icon');
-                editButton.setAttribute('icon', 'create-outline'); 
-                editButton.onclick = function() {
-                    // Aquí puedes agregar la lógica para modificar la tarea
-                    console.log('Modificando tarea:', task);
+                var updateButton = document.createElement('ons-icon');
+                updateButton.setAttribute('icon', 'fa-cog');
+                updateButton.style.color = 'blue';
+                updateButton.onclick = function() {
+                    // mandamos los campos de titulo y descripcion a la ventana modal
+
+                    //traemos el id
+                    id = task.id;
+                    console.log(id);
+                    document.getElementById("task-title").value = task.title;
+                    document.getElementById("task-description").value = task.description;
+                    document.getElementById("task-date").value = task.date;
+
+                    
+                    //le cambiamos el nombre al boton
+                    document.getElementById("button-add-task").textContent = "Modificar";
+                    // mostramos la ventana modal
+                    showEditModal(id);
                 };
-                // Agregar el botón al elemento de lista
-                newElement.appendChild(editButton);
+
+                // Crear el botón de eliminar
+                var delbutton = document.createElement('ons-icon');
+                delbutton.setAttribute('icon', 'fa-trash'); 
+                delbutton.style.color = 'red';
+                delbutton.style.marginLeft = '10px'; // Para separar los botones
+                delbutton.onclick = function() {
+                    handleDeleteTask(task.id);
+                };
+
+                // Agregar los botones al contenedor
+                iconContainer.appendChild(updateButton);
+                iconContainer.appendChild(delbutton);
+
+
+                //queda muy junto y le meto un margin de 10px y deja de listar 
+                // Agregar el contenedor al elemento de lista
+                newDiv2.appendChild(iconContainer);
+                newElement.appendChild(newDiv2);
                 list.appendChild(newElement);
             });
         } else {
@@ -46,16 +92,30 @@ async function showTasks(tasks) {
     } catch (error) {
         console.error(error);
     }
-   
 }
+
 document
         .getElementById("button-task-new")
-        .addEventListener("click", showModal);
+        .addEventListener("click", showCreateModal);
         
 
-function showModal() {
+function showCreateModal() {
     var modal = document.querySelector('ons-modal');
+    modalMode = 'create';
     modal.show();
+}
+function showEditModal()  {
+    var modal = document.querySelector('ons-modal');
+    modalMode = 'edit';
+    modal.show();
+}
+function handleTaskAction() {
+    if (modalMode === 'create') {
+        handleCreateTask();
+    } else if (modalMode === 'edit') {
+
+        handleEditTask(id);
+    }
 }
 async function createTask({ name, description, date, latitude, longitude, altitude, accuracy, altitudeAccuracy, heading, speed, timestamp }) {
     try {
@@ -87,8 +147,6 @@ async function createTask({ name, description, date, latitude, longitude, altitu
     }
 }
 
-
-
 async function handleCreateTask() {
     const name = document.getElementById("task-title").value;
     const description = document.getElementById("task-description").value;
@@ -100,7 +158,6 @@ async function handleCreateTask() {
                 console.error('Error al obtener la ubicación:', error);
                 // Aquí puedes manejar el error si ocurriera al obtener la ubicación
             } else {
-                console.log('Ubicación obtenida:', ubicacion);
                 const { latitude, longitude, altitude, accuracy, altitudeAccuracy, heading, speed, timestamp } = ubicacion;
                 createTask({ name, description, date, latitude, longitude, altitude, accuracy, altitudeAccuracy, heading, speed, timestamp });
                 alert("Tarea creada exitosamente");
@@ -112,7 +169,9 @@ async function handleCreateTask() {
     }
 }
 
-document.getElementById("button-add-task").addEventListener("click",  handleCreateTask)
+
+
+document.getElementById("button-add-task").addEventListener("click",  handleTaskAction);
 
 function getLocation(callback) {
     if (navigator.geolocation) {
@@ -151,4 +210,81 @@ function getLocation(callback) {
     }
 }
 
+async function updateTask({ id, name, description, date, latitude, longitude, altitude, accuracy, altitudeAccuracy, heading, speed, timestamp }){
+    try {
+        const url = 'http://localhost:3050/api/v1/tasks/' + id;
+        const body = JSON.stringify({
+            title: name,
+            description: description,
+            latitude: latitude,
+            logitude: longitude,
+            altitude: altitude,
+            accuracy: accuracy,
+            altitude_accuracy: altitudeAccuracy,
+            heading: heading,
+            speed: speed,
+            timestamp: timestamp
+        });
+        const response = await fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJqdWFuQGdtYWlsLmNvbSIsInVzZXJuYW1lIjoianVhbml0bzIiLCJwYXNzd29yZCI6IiQyYSQxMCR4cDhlLjN5cEtObjExM1k0Z0YvLjd1ZUR2R3QxckMvenB2cVY5b3pISWtsRTJDMmxDYklJeSIsInN0YXRlIjoxLCJpYXQiOjE3MTU0MzgyMjB9.WdseGLkZNOhppWrj23Mcd5hRxlayAGdmxJIcgtOeJtI'
+            },
+            body: body
+        });
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function handleEditTask(id){
+
+    const name = document.getElementById("task-title").value;
+    const description = document.getElementById("task-description").value;
+    const date = document.getElementById("task-date").value;
+
+    if (name.trim() !== '' && description.trim() !== '' && date.trim() !== '') {
+        getLocation(function(error, ubicacion) {
+            if (error) {
+                console.error('Error al obtener la ubicación:', error);
+                // Aquí puedes manejar el error si ocurriera al obtener la ubicación
+            } else {
+                const { latitude, longitude, altitude, accuracy, altitudeAccuracy, heading, speed, timestamp } = ubicacion;
+                updateTask({ id, name, description, date, latitude, longitude, altitude, accuracy, altitudeAccuracy, heading, speed, timestamp });
+                alert("Tarea modificada exitosamente");
+                window.location.reload();
+            }
+        });
+    } else {
+        alert("Todos los campos son requeridos");
+    }
+    
+}
+async function deleteTask(id){
+    try {
+        const url = 'http://localhost:3050/api/v1/tasks/' + id;
+        const response = await fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJqdWFuQGdtYWlsLmNvbSIsInVzZXJuYW1lIjoianVhbml0bzIiLCJwYXNzd29yZCI6IiQyYSQxMCR4cDhlLjN5cEtObjExM1k0Z0YvLjd1ZUR2R3QxckMvenB2cVY5b3pISWtsRTJDMmxDYklJeSIsInN0YXRlIjoxLCJpYXQiOjE3MTU0MzgyMjB9.WdseGLkZNOhppWrj23Mcd5hRxlayAGdmxJIcgtOeJtI'
+            }
+        });
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+function handleDeleteTask(id){
+
+    if(confirm("¿Estás seguro de eliminar la tarea?")){
+        deleteTask(id);
+        alert("Tarea eliminada exitosamente");
+        window.location.reload();
+    }
+}
 
